ecc81173224076f0fb6f768397c83411ed07ec02
maxmarkov <maxmarkov@gmail.com>
2022-07-23 21:25:13 +0200

    mc plot

--- monte-carlo.py
+++ monte-carlo.py
@@ -2,6 +2,17 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+def drift_distribution(r_median, r_mean, sigma, T):
+    """ Get git distribution parameters 
+    r_median (float): median index return
+    r_mean (float): expected index return
+    simga (float): stock volatility
+    T (int): period of time in years
+    """
+    mu_drift = (np.log(1.+r_median) + 0.5 * sigma**2 * T) / T
+    sigma_drift = np.sqrt(2. * np.log(1. + r_mean) - 2.* mu_drift * T) / T
+    return mu_drift, sigma_drift
+
 def price_model(t: int, mu_b: float, sigma_b: float, sigma: float) -> float:
     """ """
     s0 = 1. # initial price
@@ -14,21 +25,45 @@ def stock_price(T: int, mu_b: float, sigma_b: float, sigma: float):
         s = price_model(t, mu_b, sigma_b, sigma)
     return s
 
+def plot_data(rate_p, rate_a, filename=None):
+    """ """
+    assert len(rate_p) == len(rate_a)
+    x = range(1,len(rate_a)+1)
+    plt.plot(x, rate_p, c='k', label='passive')
+    plt.plot(x, rate_a, c='orange', label='active')
+
+    plt.xlabel("Sub-portfolio size")
+    plt.ylabel("Rate")
+
+    plt.xlim([0,20])
+    plt.ylim([0.2,0.7])
+    plt.xticks([0, 5, 10, 15, 20])
+
+    plt.grid(visible=True, alpha=0.5)
+    plt.legend()
+
+    plt.title( "Sub-portfolio vs Index")
+    if filename is not None:
+        plt.savefig(filename)
+    else:
+        plt.show()
 
 
-N_MC = 10000          # number of Monte Carlo trials, N_MC = 10000 in the paper
+N_MC = 100          # number of Monte Carlo trials, N_MC = 10000 in the paper
 
 T = 5                # period in years
 N = 500              # number of stocks
 sigma = 0.2          # generic annual stock volatility 
-mu_drift = 0.04      # drift mean
-sigma_drift = 0.13   # drift variance 
+r_median = 0.1       # median index return
+r_mean = 0.5         # mean index return
+
+mu_drift, sigma_drift = drift_distribution(r_median, r_mean, sigma, T)
 
 
 rate_passive = []
 rate_active = []
 
-for n_pfl in range(1,5):
+for n_pfl in range(1,21):
     print(f'Sub-portfolio size {n_pfl}')
 
     N_active = 0
@@ -51,14 +86,4 @@ for n_pfl in range(1,5):
     rate_passive.append(N_passive/N_MC)
     rate_active.append(N_active/N_MC)
 
-plt.plot(rate_passive, c='k', label='passive')
-plt.plot(rate_active, c='orange', label='active')
-#plt.legend(np.round(sigma, 2))
-plt.xlabel("Sub-portfolio size")
-plt.ylabel("Rate")
-plt.xlim([0,20])
-plt.ylim([0,1])
-plt.legend()
-#plt.title( "Realizations of Geometric Brownian Motion with different variances\n $\mu=1$")
-plt.savefig("figure.png")
-#plt.show()
+plot_data(rate_p=rate_passive, rate_a=rate_active, filename='figure.png')
